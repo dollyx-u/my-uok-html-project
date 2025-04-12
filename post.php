a8d2dca732d60faf63e8cdd860f0e2c1de8f8ba0
dollyx-u <uwasedorcas22@gmail.com>
2025-04-12 19:55:26 +0200

    Add files via upload

---
+++ post.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h1>GET Method Form with Password</h1>
+    <form action="get.php" method="get">
+        <label for="name">Enter your name:</label>
+        <input type="text" id="name" name="name"><br><br>
+
+        <label for="age">Enter your age:</label>
+        <input type="number" id="age" name="age"><br><br>
+
+        <label for="password">Enter your password:</label>
+        <input type="password" id="password" name="password"><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
